f4afca5bf6f8fb24df3bf4476d2030e57c3325cf
JoeGuillory <joeguillory15@gmail.com>
2025-10-04 17:44:20 -0500

    ui prefab

---
+++ Assets/Scripts/Cauldron.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Cauldron : MonoBehaviour
+{
+    PotionType Slot1;
+    PotionType Slot2;
+    PotionType Slot3;
+
+    
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+
+
+    }
+
+}
